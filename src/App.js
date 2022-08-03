2de147d8c430964f3eff73e2e3440e5124dbe892
Paik Jay <51130958+PaikJay@users.noreply.github.com>
2022-08-03 15:20:32 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+      <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <div>
+        <h2>Pole Vert</h2>
+        <p></p>
+      </div>
+   </div>
+  
+ 
+    )
+  }
+}
